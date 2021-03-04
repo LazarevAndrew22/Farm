0e2e6c70e1d16dffbad032a9b5348333ec9c2493
LazarevAndrew22 <andrey20010012@gmail.com>
2021-03-04 18:43:11 +0200

    Adding animals here and collecting products. Also also display the result

--- main.php
+++ main.php
@@ -1,2 +1,31 @@
 <?php
 
+spl_autoload_register(function($class)
+{
+    $path = str_replace('\\', '/', $class. '.php');
+    if (file_exists($path)){
+        require_once $path;
+    }
+});
+
+
+$barn = new Barn();
+
+$myFarm = new Farm($barn);
+
+for ($i=0;$i<20;$i++) {
+    $myFarm->addAnimal(new Chicken()); //add chickens
+}
+
+for ($i=0;$i<10;$i++) {
+    $myFarm->addAnimal(new Cow()); //add cows
+}
+
+
+$myFarm->collectProducts(); //collecting products
+
+
+//results ->
+echo "Milk collected " . $myFarm->returnMilk(). PHP_EOL;
+echo "Eggs collected " .$myFarm->returnEggs(). PHP_EOL;
+
